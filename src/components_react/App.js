db6ab8a677e32a2fb2c549db00614a2cd34224dc
NatalyaDol <suhoikova@gmail.com>
2019-12-12 16:23:14 +0300

    Project start

---
+++ src/components_react/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>There is a headline at the  app.js file - !!!REACT JS is working</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/store/example.js
@@ -0,0 +1,12 @@
+export default {
+  state: {
+    message: 'There is a text from example.js vue - !!!VUE is working'
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getMessage (state) {
+      return state.message
+    }
+  }
+}
